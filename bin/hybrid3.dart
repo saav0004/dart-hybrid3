0f29d3f8ba173b1713d9a6563de8e87b0e0c6916
Luciano Saavedra <saav0004@algonquinlive.com>
2023-10-12 14:19:46 -0400

    somewhat completed

--- bin/hybrid3.dart
+++ bin/hybrid3.dart
@@ -1,5 +1,38 @@
+import 'dart:convert';
+// import Student class
 import 'package:hybrid3/hybrid3.dart' as hybrid3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${hybrid3.calculate()}!');
+  String jsonStudents = '''
+[
+  {"first":"Steve", "last":"Griffith", "email":"griffis@algonquincollege.com"},
+  {"first":"Adesh", "last":"Shah", "email":"shaha@algonquincollege.com"},
+  {"first":"Tony", "last":"Davidson", "email":"davidst@algonquincollege.com"},
+  {"first":"Adam", "last":"Robillard", "email":"robilla@algonquincollege.com"}
+]
+''';
+
+  List<dynamic> jsonList = json.decode(jsonStudents);
+
+  List<Map<String, String>> resultMapList = jsonList.map((item) {
+    Map<String, String> resultMap = {};
+    if (item is Map<String, dynamic>) {
+      item.forEach((key, value) {
+        if (value is String) {
+          resultMap[key] = value;
+        }
+      });
+    }
+    return resultMap;
+  }).toList();
+
+  hybrid3.Student student = hybrid3.Student(resultMapList);
+  student.plus({
+    'first': 'Luciano',
+    'last': 'Aavedra',
+    'email': 'saav0004@algonquin.com'
+  });
+  student.sort('last');
+  student.remove('first', "Adesh");
+  student.output();
 }
--- lib/hybrid3.dart
+++ lib/hybrid3.dart
@@ -1,3 +1,25 @@
-int calculate() {
-  return 6 * 7;
+class Student {
+  late List<Map<String, String>> people;
+
+  Student(List<Map<String, String>> initialData) {
+    people = initialData;
+  }
+
+  void sort(String field) {
+    people.sort((a, b) => a[field]!.compareTo(b[field]!));
+  }
+
+  output() {
+    for (var student in people) {
+      print(student);
+    }
+  }
+
+  void plus(Map<String, String> person) {
+    people.add(person);
+  }
+
+  void remove(String field, String value) {
+    people.removeWhere((element) => element[field] == value);
+  }
 }
